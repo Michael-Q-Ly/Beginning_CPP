271ebb5b0b83922d067d38a4e469d1e1fa8598b5
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-19 14:29:31 -0600

    011: Section Challenge - Refactor code and add helper functions

--- Section11/SectionChallenge/src/main.cpp
+++ Section11/SectionChallenge/src/main.cpp
@@ -4,16 +4,23 @@
 #include <iostream>
 #include <vector>
 
+// Function handles
 void display_menu		( void ) ;
 char get_user_selection		( void ) ;
-void print_numbers		( std::vector <double> const userVector ) ;
-void add_number			( std::vector <double> &userVector ) ;
-double calc_mean		( std::vector <double> const &userVector ) ;
-void display_mean		( std::vector <double> const &userVector ) ;
-void display_smallest_number 	( std::vector <double> const &userVector ) ;
-void display_largest_number 	( std::vector <double> const &userVector ) ;
-void display_error		( void ) ;
-void exit_program		( void ) ;
+void handle_display		( std::vector <double> const &userVector ) ;
+void handle_add_number		( std::vector <double> &userVector ) ;
+void handle_mean		( std::vector <double> const &userVector ) ;
+void handle_smallest_number 	( std::vector <double> const &userVector ) ;
+void handle_largest_number 	( std::vector <double> const &userVector ) ;
+void handle_invalid_choice	( void ) ;
+void handle_exit_program	( void ) ;
+
+// Function prototypes that do calculations
+void get_display		( std::vector <double> const &userVector ) ;
+double get_mean			( std::vector <double> const &userVector ) ;
+double get_smallest		( std::vector <double> const &userVector ) ;
+double get_largest		( std::vector <double> const &userVector ) ;
+
 
 int main() {
 
@@ -27,15 +34,15 @@ int main() {
 		selection = get_user_selection() ;
 		// Decide what the selection does
 		switch ( selection ) {
-			case 'P':	print_numbers( userVector ) ;		break ;
-			case 'A':	add_number( userVector ) ;		break ;
-			case 'M':	display_mean( userVector ) ;		break ;
-			case 'S':	display_smallest_number( userVector ) ;	break ;
-			case 'L':	display_largest_number( userVector ) ;	break ;
-			case 'Q':	exit_program() ;			break ;
-			default:	display_error() ;
+			case 'P':	handle_display( userVector ) ;		break ;
+			case 'A':	handle_add_number( userVector ) ;	break ;
+			case 'M':	handle_mean( userVector ) ;		break ;
+			case 'S':	handle_smallest_number( userVector ) ;	break ;
+			case 'L':	handle_largest_number( userVector ) ;	break ;
+			case 'Q':	handle_exit_program() ;			break ;
+			default:	handle_invalid_choice() ;
 		}
-	} while ( selection != 'q' && selection != 'Q' ) ;
+	} while ( selection != 'Q' ) ;
 
 	std::cout << std::endl ;
 	return 0 ;
@@ -75,13 +82,9 @@ char get_user_selection( void ) {
  * If the user chooses 'p' or 'P', show the list,
  * else show an error if the list is empty
  */
-void print_numbers( std::vector <double> const userVector ) {
+void handle_display( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
-		std::cout << "[ " ;
-		for ( auto num : userVector ) {
-			std::cout << num << " " ;
-		}
-		std::cout << "] " << std::endl ;
+		get_display( userVector ) ;
 	}
 	else {
 		std::cerr << "[] - The list is empty!" << std::endl << std::flush ;
@@ -92,7 +95,7 @@ void print_numbers( std::vector <double> const userVector ) {
  * If the user chooses 'a' or 'A', allow them to add a number to the list.
  * Display an error if it is not a number.
  */
-void add_number( std::vector <double> &userVector ) {
+void handle_add_number( std::vector <double> &userVector ) {
 	double user_num {} ;
 	std::cout << "Enter a number: " ;
 	while ( !( std::cin >> user_num ) ) {
@@ -107,26 +110,14 @@ void add_number( std::vector <double> &userVector ) {
 	std::cout << "\nNumber added!" << std::endl ;
 }
 
-/**
- * Calculate the mean of the nonempty list
- */
-double calc_mean( std::vector <double> const &userVector ) {
-	double sum {} ;
-	for ( auto num : userVector ) {
-		sum += num ;
-	}
-	return sum / userVector.size() ;
-}
 	
 /**
  * If the user chooses 'm' or 'M' in a nonempty list,
  * get the mean then show it, else show an error
  */
-void display_mean( std::vector <double> const &userVector ) {
+void handle_mean( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
-		double average ;
-		average = calc_mean( userVector ) ;
-		std::cout << "The mean of your numbers is " << average << std::endl ;
+		std::cout << "The mean of your numbers is " << get_mean( userVector ) << std::endl ;
 	}
 	else {
 		std::cerr << "Unable to calculate the mean - no data!\n" << std::endl ;
@@ -137,18 +128,9 @@ void display_mean( std::vector <double> const &userVector ) {
  * If the user chooses 's' or 'S', show the smallest number,
  * else show an error if the list is empty
  */
-void display_smallest_number ( std::vector <double> const &userVector ) {
+void handle_smallest_number ( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
-		double smallest {userVector.at( 0 )} ;
-		for ( auto num : userVector ) {
-			if ( num >= smallest ) {
-				continue ;
-			}
-			else {
-				smallest = num ;
-			}
-		}
-		std::cerr << "The smallest number is " << smallest << std::endl ;
+		std::cerr << "The smallest number is " << get_smallest( userVector ) << std::endl ;
 	}
 	else {
 		std::cout << "Unable to determine the smallest number - list is empty!" << std:: endl ;
@@ -159,18 +141,9 @@ void display_smallest_number ( std::vector <double> const &userVector ) {
  * If the user chooses 'l' or 'L', show the largest number,
  * else show an error if the list is empty
  */
-void display_largest_number ( std::vector <double> const &userVector ) {
+void handle_largest_number ( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
-		double largest {userVector.at( 0 )} ;
-		for ( auto num : userVector ) {
-			if ( num <= largest ) {
-				continue ;
-			}
-			else {
-				largest = num ;
-			}
-		}
-		std::cout << "The largest number is " << largest << std::endl ;
+		std::cout << "The largest number is " << get_largest( userVector ) << std::endl ;
 	}
 	else {
 		std::cerr << "Unable to determine the largest number - list is empty!" << std:: endl ;
@@ -181,7 +154,7 @@ void display_largest_number ( std::vector <double> const &userVector ) {
  * If the user chooses an option not in the list,
  * display an error
  */
-void display_error( void ) {
+void handle_invalid_choice( void ) {
 	std::cerr << "Invalid -- try again!" << std::endl ;
 }
 
@@ -189,7 +162,69 @@ void display_error( void ) {
  * If the user chooses 'q' or 'Q',
  * show a quit message
  */
-void exit_program( void ) {
+void handle_exit_program( void ) {
 	std::cout << "\nGoodbye!" << std::endl ;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//					Helper Functions					//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Go through the vector and display every number
+ */
+void get_display( std::vector <double> const &userVector ) {
+	std::cout << "[ " ;
+	for ( auto num : userVector ) {
+		std::cout << num << " " ;
+	}
+	std::cout << "] " << std::endl ;
+}
+
+/**
+ * Calculate the mean of the nonempty list
+ */
+double get_mean( std::vector <double> const &userVector ) {
+	double sum {} ;
+	for ( auto num : userVector ) {
+		sum += num ;
+	}
+	return sum / userVector.size() ;
+}
+
+/**
+ * Parse the vector and perform swaps to obtain the
+ * smallest number in the vector
+ */
+double get_smallest( std::vector <double> const &userVector ) {
+	double smallest {userVector.at( 0 )} ;
+	for ( auto num : userVector ) {
+		if ( num >= smallest ) {
+			continue ;
+		}
+		else {
+			smallest = num ;
+		}
+	}
+
+	return smallest ;
+}
+
+/**
+ * Parse the vector and perform swaps to obtain the
+ * largest number in the vector
+ */
+double get_largest ( std::vector <double> const &userVector ) {
+	double largest {userVector.at( 0 )} ;
+	for ( auto num : userVector ) {
+		if ( num <= largest ) {
+			continue ;
+		}
+		else {
+			largest = num ;
+		}
+	}
+
+	return largest ;
 }
 
