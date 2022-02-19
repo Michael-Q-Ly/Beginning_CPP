4eac476e4fee353c2e4e532bd1fe01357844b28e
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-19 13:01:28 -0600

    011: Section Challenge - Edit get_user_selection() to return an      uppercase letter to avoid switch-case fall-through

--- Section11/SectionChallenge/src/main.cpp
+++ Section11/SectionChallenge/src/main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 void display_menu		( void ) ;
-void get_user_selection		( char &selection ) ;
+char get_user_selection		( void ) ;
 void print_numbers		( std::vector <double> const userVector ) ;
 void add_number			( std::vector <double> &userVector ) ;
 double calc_mean		( std::vector <double> const &userVector ) ;
@@ -24,20 +24,14 @@ int main() {
 		// Display a menu to the user
 		display_menu() ;
 		// Get the user's option
-		get_user_selection( selection ) ;
+		selection = get_user_selection() ;
 		// Decide what the selection does
 		switch ( selection ) {
-			case 'p':
 			case 'P':	print_numbers( userVector ) ;		break ;
-			case 'a':
 			case 'A':	add_number( userVector ) ;		break ;
-			case 'm':
 			case 'M':	display_mean( userVector ) ;		break ;
-			case 's':
 			case 'S':	display_smallest_number( userVector ) ;	break ;
-			case 'l':
 			case 'L':	display_largest_number( userVector ) ;	break ;
-			case 'q':
 			case 'Q':	exit_program() ;			break ;
 			default:	display_error() ;
 		}
@@ -50,7 +44,9 @@ int main() {
 //					Function Definitions					//
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-/* Displays the menu */
+/**
+ * Displays the menu
+ */
 void display_menu( void ) {
 	std::cout << "\n---------------------------------\n" ;
 	std::cout << "P - Print numbers" << std::endl ;
@@ -61,14 +57,24 @@ void display_menu( void ) {
 	std::cout << "Q - Quit" << std::endl ;
 }
 
-/* Asks the user to enter a choice from the menu */
-void get_user_selection( char &selection ) {
+/**
+ * Asks the user to enter a choice from the menu
+ */
+char get_user_selection( void ) {
+	char selection {} ;
 	std::cout << "\nEnter your choice: " ;
 	std::cin  >> selection ;
 	std::cout << std::endl ;
+
+	// Return the selection as an uppercase letter
+	// so that we avoid fall-through
+	return std::toupper( selection ) ;
 }
 
-/* If the user chooses 'p' or 'P', show the list, else show an error if the list is empty */
+/**
+ * If the user chooses 'p' or 'P', show the list,
+ * else show an error if the list is empty
+ */
 void print_numbers( std::vector <double> const userVector ) {
 	if ( userVector.size() ) {
 		std::cout << "[ " ;
@@ -82,7 +88,10 @@ void print_numbers( std::vector <double> const userVector ) {
 	}
 }
 
-/* If the user chooses 'a' or 'A', allow them to add a number to the list. Display an error if it is not a number. */
+/**
+ * If the user chooses 'a' or 'A', allow them to add a number to the list.
+ * Display an error if it is not a number.
+ */
 void add_number( std::vector <double> &userVector ) {
 	double user_num {} ;
 	std::cout << "Enter a number: " ;
@@ -98,7 +107,9 @@ void add_number( std::vector <double> &userVector ) {
 	std::cout << "\nNumber added!" << std::endl ;
 }
 
-/* Calculate the mean of the nonempty list */
+/**
+ * Calculate the mean of the nonempty list
+ */
 double calc_mean( std::vector <double> const &userVector ) {
 	double sum {} ;
 	for ( auto num : userVector ) {
@@ -107,7 +118,10 @@ double calc_mean( std::vector <double> const &userVector ) {
 	return sum / userVector.size() ;
 }
 	
-/* If the user chooses 'm' or 'M' in a nonempty list, get the mean then show it, else show an error */
+/**
+ * If the user chooses 'm' or 'M' in a nonempty list,
+ * get the mean then show it, else show an error
+ */
 void display_mean( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
 		double average ;
@@ -119,7 +133,10 @@ void display_mean( std::vector <double> const &userVector ) {
 	}
 }
 
-/* If the user chooses 's' or 'S', show the smallest number, else show an error if the list is empty */
+/**
+ * If the user chooses 's' or 'S', show the smallest number,
+ * else show an error if the list is empty
+ */
 void display_smallest_number ( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
 		double smallest {userVector.at( 0 )} ;
@@ -138,7 +155,10 @@ void display_smallest_number ( std::vector <double> const &userVector ) {
 	}
 }
 
-/* If the user chooses 'l' or 'L', show the largest number, else show an error if the list is empty */
+/**
+ * If the user chooses 'l' or 'L', show the largest number,
+ * else show an error if the list is empty
+ */
 void display_largest_number ( std::vector <double> const &userVector ) {
 	if ( userVector.size() ) {
 		double largest {userVector.at( 0 )} ;
@@ -157,12 +177,18 @@ void display_largest_number ( std::vector <double> const &userVector ) {
 	}
 }
 
-/* If the user chooses an option not in the list, display an error */
+/**
+ * If the user chooses an option not in the list,
+ * display an error
+ */
 void display_error( void ) {
 	std::cerr << "Invalid -- try again!" << std::endl ;
 }
 
-/* If the user chooses 'q' or 'Q', show a quit message */
+/**
+ * If the user chooses 'q' or 'Q',
+ * show a quit message
+ */
 void exit_program( void ) {
 	std::cout << "\nGoodbye!" << std::endl ;
 }
