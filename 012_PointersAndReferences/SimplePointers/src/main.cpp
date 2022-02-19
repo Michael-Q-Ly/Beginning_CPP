6f3a15659d20fe08e9159d4448bb80be942ddccd
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-19 15:15:03 -0600

    012: Add SimplePointer example

---
+++ 012_PointersAndReferences/SimplePointers/src/main.cpp
@@ -0,0 +1,50 @@
+// Section 12
+// Simple Pointers
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+	int num {10} ;
+	std::cout << "Value of num is: " << num << std::endl ;
+	std::cout << "sizeof num is: " << sizeof( num ) << std::endl ;
+	std::cout << "Address of num is: " << &num << std::endl ;
+
+	int *p ;
+	std::cout << "\nValue of p is: " << p << std::endl ;			// Garbage
+	std::cout << "Address of p is: " << &p << std::endl ;
+	std::cout << "sizeof p is: " << sizeof( p ) << std::endl ;
+
+	std::cout << "\nValue of p is: " << p << std::endl ;
+
+	int *p1 {nullptr} ;
+	double *p2 {nullptr} ;
+	unsigned long long *p3 {nullptr} ;
+	std::vector <std::string> *p4 {nullptr} ;
+	std::string *p5 {nullptr} ;
+
+	std::cout << "\nsizeof p1 is: " << sizeof( p1 ) << std::endl ;
+	std::cout << "sizeof p2 is: " << sizeof( p2 ) << std::endl ;
+	std::cout << "sizeof p3 is: " << sizeof( p3 ) << std::endl ;
+	std::cout << "sizeof p4 is: " << sizeof( p4 ) << std::endl ;
+	std::cout << "sizeof p5 is: " << sizeof( p5 ) << std::endl ;
+
+	int score {10} ;
+	double high_temp {100.7} ;
+
+	int *score_ptr {nullptr} ;
+
+	score_ptr = &score ;
+	std::cout << "Value of score is: " << score << std::endl ;
+	std::cout << "Address of score is: " << &score << std::endl ;
+	std::cout << "Value of score_ptr is: " << score_ptr << std::endl ;
+	
+	( void )( high_temp ) ;
+//	score_ptr = &high_temp ;						// Compiler error! score_ptr expects type int, not double
+
+	std::cout << std::endl ;
+	return 0 ;
+}
+
