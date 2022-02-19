c32f71ce3083e601a292f71092050db02e446daa
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-19 15:29:50 -0600

    012: Add Dereference example

---
+++ 012_PointersAndReferences/Dereference/src/main.cpp
@@ -0,0 +1,54 @@
+// Section 12
+// Dereferencing
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+
+	int score {100} ;
+	int *score_ptr {&score} ;
+
+	std::cout << *score_ptr << std::endl ;					// 100
+
+	*score_ptr = 200 ;
+
+	std::cout << *score_ptr << std::endl ;					// 200
+	std::cout << score << std::endl ;					// 200
+
+	std::cout << "\n------------------------" << std::endl ;
+	double high_temp {100.7} ;
+	double low_temp {37.4} ;
+	double *temp_ptr {&high_temp} ;
+
+	std::cout << *temp_ptr << std::endl ;					// 100.7
+	temp_ptr = &low_temp ;
+	std::cout << *temp_ptr << std::endl ;					// 37.4
+
+	std::cout << "\n------------------------" << std::endl ;
+	std::string name {"Michael"} ;
+	std::string *string_ptr {&name} ;
+
+	std::cout << *string_ptr << std::endl ;					// Michael
+	name = "James" ;
+	std::cout << *string_ptr << std::endl ;					// James
+
+	std::cout << "\n------------------------" << std::endl ;
+	std::vector <std::string> stooges {"Larry", "Moe", "Curly"} ;
+	std::vector <std::string> *vector_ptr {nullptr} ;
+
+	vector_ptr = &stooges ;
+
+	std::cout << "First stooge: " << (*vector_ptr).at( 0 ) << std::endl ;	// Larry
+
+	std::cout << "Stooges: " ;
+	for ( auto stooge : *vector_ptr ) {
+		std::cout << stooge << " " ;
+	}
+	std::cout << std::endl ;
+
+	std::cout << std::endl ;
+	return 0 ;
+}
+
