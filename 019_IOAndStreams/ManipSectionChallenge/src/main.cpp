d087990e0eb00b8d54994f03254d4342e157350b
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-11-13 14:56:39 -0600

    019: Add my solution to ManipSectionChallenge

--- 019_IOAndStreams/ManipSectionChallenge/src/main.cpp
+++ 019_IOAndStreams/ManipSectionChallenge/src/main.cpp
@@ -20,6 +20,15 @@
 #include <string>
 #include <vector>
 
+/* #define DEBUG */
+/* #define SHOW_UNFORMATTED */
+
+short const TOTAL_WIDTH      {80} ;
+short const COUNTRY_WIDTH    {20} ;
+short const CITY_WIDTH       {20} ;
+short const POPULATION_WIDTH {20} ;
+short const COST_WIDTH       {20} ;
+
 struct City {
 	std::string name ;
 	long population ;
@@ -36,7 +45,58 @@ struct Tours {
 	std::vector<Country> countries ;
 } ;
 
+#ifdef DEBUG
+void ruler( void ) ;
+#endif /* DEBUG */
+
+struct Tours init_tours( void ) ;
+void print_unformatted( struct Tours tours ) ;
+void print_centered_title( struct Tours tours ) ;
+void print_tour_labels( void ) ;
+void print_tour_info( struct Tours tours ) ;
+
+
 int main( int argc, char *argv[] ) {
+	struct Tours tours = init_tours() ;
+
+#ifdef SHOW_UNFORMATTED
+	print_unformatted( tours ) ;
+#endif /* SHOW_UNFORMATTED */
+
+#ifdef DEBUG
+	for ( size_t i {} ; i < 8 ; i++ ) {
+		ruler() ;
+	}
+#endif /* DEBUG */
+	
+	// Print tour information
+	print_centered_title( tours ) ;
+	print_tour_labels() ;
+	print_tour_info( tours ) ;
+
+	std::cout << std::endl ;
+	return 0 ;
+}
+
+#ifdef DEBUG
+/* ----------------------------------------------------------------------------*/
+/**
+ * @brief	A string of numbers to debug spacing
+ */
+/* ------------------------------------------------------------------------------------*/
+void ruler( void ) {
+	std::cout << "1234567890" ;
+}
+#endif /* DEBUG */
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @brief		Populate/initialize the Tours structure
+ *
+ * @return Tours	Tours struct
+ */
+/* ------------------------------------------------------------------------------------*/
+struct Tours init_tours( void ) {
 	struct Tours tours {
 		"Tour Ticket Prices from Miami", {
 			{
@@ -69,18 +129,80 @@ int main( int argc, char *argv[] ) {
 		} /* End Tours */
 	} ;
 
+	return tours ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @brief		Unformatted output of tours
+ *
+ * @param tours		Struct of tours
+ */
+/* ------------------------------------------------------------------------------------*/
+void print_unformatted( struct Tours tours ) {
 	// Unformatted display so you can see how to access the vector elements
+	std::cout << "Unformatted display\n" << std::endl ;
 	std::cout << tours.title << std::endl ;
 	for ( auto country : tours.countries ) { // Loop through the countries
 		std::cout << country.name << std::endl ;
-		for ( auto city : country.cities ) {	// Loop t hrough the cities for each country
+		for ( auto city : country.cities ) {	// Loop through the cities for each country
 			std::cout << "\t" << city.name
 				  << "\t" << city.population
 				  << "\t" << city.cost
 				  << std::endl ;
 		}
 	}
+	std::cout << "--------------------------------------------------------------------------------" << std::endl ;
+}
 
-	std::cout << std::endl ;
-	return 0 ;
+/* ----------------------------------------------------------------------------*/
+/**
+ * @brief		Print the tour title centered above the labels row
+ *
+ * @param tours		Struct of tours
+ */
+/* ------------------------------------------------------------------------------------*/
+void print_centered_title( struct Tours tours ) {
+	std::cout << std::endl << std::endl ;
+
+	int const CENTER = ( TOTAL_WIDTH - tours.title.length() ) / 2 ;
+
+	std::cout << std::setw( CENTER ) << std::setfill(' ') << ' ' ;
+	std::cout << tours.title << std::endl ;
+}
+
+void print_tour_labels( void ) {
+	std::cout << std::setw( COUNTRY_WIDTH + 1 )	<< std::left		<< "\nCountry" ;	// \n counts as a char in setw
+	std::cout << std::setw( CITY_WIDTH )					<< "City" ;
+	std::cout << std::setw( POPULATION_WIDTH )	<< std::right		<< "Population" ;
+	std::cout << std::setw( COST_WIDTH )		<< "Price (USD)"	<< std::endl ;
+	std::cout << "--------------------------------------------------------------------------------" << std::endl ;
+}
+
+/* ----------------------------------------------------------------------------*/
+/**
+ * @brief		Print the tour information
+ *
+ * @param tours		Struct of tours
+ */
+/* ------------------------------------------------------------------------------------*/
+void print_tour_info( struct Tours tours ) {
+	for ( auto country : tours.countries ) { // Loop through the countries
+		std::cout << std::setw( COUNTRY_WIDTH ) << std::left << country.name ;
+		for ( auto city : country.cities ) {	// Loop through the cities for each country
+			if ( city.name != country.cities.at( 0 ).name ) {
+				std::cout << std::setw( COUNTRY_WIDTH )		<< std::setfill(' ')	<< "" ;
+				std::cout << std::setw( CITY_WIDTH )		<< std::left		<< city.name
+					  << std::setw( POPULATION_WIDTH )	<< std::right		<< city.population
+					  << std::setw( COST_WIDTH )		<< std::fixed		<< std::setprecision( 2 ) << city.cost
+					  << std::endl ;
+			}
+			else { // First city in the list
+				std::cout << std::setw( CITY_WIDTH )		<< std::left		<< city.name
+					  << std::setw( POPULATION_WIDTH )	<< std::right		<< city.population
+					  << std::setw( COST_WIDTH )		<< std::fixed		<< std::setprecision( 2 ) << city.cost
+					  << std::endl ;
+			}
+		}
+	}
 }
